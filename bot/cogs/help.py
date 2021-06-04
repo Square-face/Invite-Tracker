861cf9c8574b49d4a5eae0b94c9b5d085407493c
SQ8 <66129065+Square-face@users.noreply.github.com>
2021-06-04 13:52:21 +0200

    creating cog help

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -55,6 +55,62 @@ class MyHelp(commands.HelpCommand):
         # send help embed
         return await ctx.send(embed=embed)
 
+    async def send_cog_help(self, cog):
+        '''Send information about a module
+        
+        Information includes: commands ond cog description'''
+        
+        # defining variables
+        ctx = self.context
+        bot = self.context.bot
+        prefix = self.clean_prefix
+        
+        if cog.qualified_name == "Jishaku" and not await bot.is_owner(ctx.author):
+            return await ctx.send(f'No command called "{cog.qualified_name}" found.')
+        
+        # creating help embed
+        embed = discord.Embed(
+            color=bot.config.Color,
+            title=f"{cog.qualified_name} help",
+            description=cog.description
+        )
+        
+        # lift of available commands 
+        command_list = []
+        
+        for cmd in cog.get_commands():
+            # go through all the commands in this cog
+            if cmd.hidden and not await bot.is_owner(ctx.author):
+                # ignore the command if it is hidden
+                continue
+            
+            
+            aliases = [cmd.qualified_name]
+            # list of all the names this command has
+            # the original name will always be added first
+            
+            for alias in cmd.aliases:
+                # go trough its aliases and add them
+                aliases.append(alias)
+            
+            # add the command to command list
+            command_list.append(f"`{prefix}[{'|'.join(aliases)}]`\n - {cmd.brief}\n")
+        
+        
+        if len(command_list) == 0:
+            # if no commands existed for this cog, ignore it and send error message
+            return await ctx.send(f'No command called "{cog.qualified_name}" found.')
+        
+        # add the commands to help embed
+        embed.add_field(
+            name="Commands",
+            value="\n".join(command_list)
+        )
+        
+        # send embed
+        return await ctx.send(embed=embed)
+        
+
 def setup(bot):
     bot._original_help_command = bot.help_command
     
